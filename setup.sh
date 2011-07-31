1cf8b76490d29fb5bbf71864918878f541d807f4
Lim Borey <borey@borey-Aspire-4740.(none)>
2011-07-31 12:32:38 +0700

    add basic environment setup for ubuntu 11.04

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Debs
+sudo apt-get update
+
+#development environment
+sudo apt-get install -y curl vim openjdk-6-jre mercurial git gedit-plugins  
+bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+
+#utility
+sudo apt-get install -y rar
+
+#entertainment app
+sudo apt-get install -y vlc vlc-plugin-pulse mozilla-plugin-vlc ubuntu-restricted-extras gstreamer0.10-ffmpeg 
+
+
+sudo apt-get upgrade -y
