280ba228c7993fb816c4ed2b9deb831fe7c5d138
borey <lim.borey@gmail.com>
2011-07-31 13:38:05 +0700

    add terminator

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y curl vim openjdk-6-jre mercurial git gedit-plugins
 bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 
 #utility
-sudo apt-get install -y rar
+sudo apt-get install -y rar terminator
 
 #entertainment app
 sudo apt-get install -y vlc vlc-plugin-pulse mozilla-plugin-vlc ubuntu-restricted-extras gstreamer0.10-ffmpeg 
